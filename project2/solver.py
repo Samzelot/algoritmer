5dd9e504b408623bb992327adccd0121b8069ac1
po3733gr-s <58813897+po3733gr-s@users.noreply.github.com>
2021-09-22 11:23:52 +0200

    testing newtons method

--- project2/solver.py
+++ project2/solver.py
@@ -1,5 +1,24 @@
+import numpy as np
+
 class Solver:
     pass
 
 class Newtons_method:
+
     pass
+
+f = lambda x1,x2: 100(x2-x1**2)**2 + (1-x1)**2
+
+def g(x):
+    pass
+
+def G(f,x):
+    n = x.shape[0]
+    pass
+    #returns Hessian
+
+
+def newtons_method(f,init_g, k = 100):
+    for i in range(k):
+        s = -np.linalg.inv(G(x))g(x)
+        x = x + s
