5e587980e840f3534ab614f417648fa3048de8c9
po3733gr-s <58813897+po3733gr-s@users.noreply.github.com>
2021-09-22 13:17:37 +0200

    Newtons Method implemented

--- project2/main.py
+++ project2/main.py
@@ -1,12 +1,14 @@
 
 from problem import Problem
-from solver import Solver
+from solver import NewtonsMethod
+import numpy as np
 
 def main():
-    f = lambda x: x**2
+    f = lambda x: 100*(x[1]-x[0]**2)**2 + (1-x[0])**2
+    #f = lambda x: np.sum(x**2)
     problem = Problem(f)
-    solver = Solver()
-    solver.solve(problem)
+    solver = NewtonsMethod('cauchy', 1e-3, 1e-5)
+    print(solver.solve(problem, np.array([1, 2])))
 
 if __name__ == "__main__":
     main()
--- project2/solver.py
+++ project2/solver.py
@@ -1,24 +1,84 @@
 import numpy as np
+import scipy.linalg as sl
+from scipy.optimize import approx_fprime
+from abc import abstractmethod, ABC
 
-class Solver:
-    pass
+class Solver(ABC):
+    
+    @abstractmethod
+    def solve(self, f):
+        pass
 
-class Newtons_method:
+class QuasiNewtonMethod(Solver):
+    
+    def __init__(self, stopping_criteria, stopping_error):
+        self.stopping_error = stopping_error
+        self.stopping_criteria = stopping_criteria
+        
+    def solve(self, problem, guess, iters=1000):
+        f = problem.f
+        g = problem.gradient or (lambda x: approx_fprime(x, f, 1e-5))
+        #TODO: add attribute for epsilon in approx_fprime function
 
-    pass
+        x = guess
+        for i in range(iters):
+            H = self.hessian(x, f)
 
-f = lambda x1,x2: 100(x2-x1**2)**2 + (1-x1)**2
+            if sl.det(H) == 0: # Check if determinant is zero
+                raise ValueError("Hessian matrix is singular")
+                
+            s = -np.linalg.inv(H)@g(x) # calculate the next step
 
-def g(x):
-    pass
+            new_x = self.step(x, s)
 
-def G(f,x):
-    n = x.shape[0]
-    pass
-    #returns Hessian
+            if self.stopping_criteria.lower() == "cauchy":
+                print(f"np.linalg.norm(x-new_x): {np.linalg.norm}, selfstoppingerror: {self.stopping_error}")
+                if np.linalg.norm(x-new_x) < self.stopping_error: #check the cauchy stopping criteria
+                    return new_x
+            elif self.stopping_criteria.lower() == "residual":
+                print(f"np.linalg.norm(g(new_x)): {np.linalg.norm(g(new_x))}, selfstoppingerror: {self.stopping_error}")
+                if np.linalg.norm(g(new_x))< self.stopping_error:
+                    return new_x
+            else:
+                raise ValueError("Not recognized stopping criterion")
+            x = new_x
+
+        raise RuntimeError("Maximum iterations exceeded")
+
+    @abstractmethod
+    def step(self, x, s):
+        return
+    
+    @abstractmethod
+    def hessian(self, x, f):
+        return
 
 
-def newtons_method(f,init_g, k = 100):
-    for i in range(k):
-        s = -np.linalg.inv(G(x))g(x)
-        x = x + s
+class NewtonsMethod(QuasiNewtonMethod):
+
+    def __init__(self, stopping_criteria, stopping_error, finite_differences_step):
+        super().__init__(stopping_criteria, stopping_error)
+        self.finite_differences_step = finite_differences_step
+        
+    def hessian(self, x, f):
+
+        n = x.shape[0] #x is a n dimensional vector (initial guess)
+
+        H = np.zeros((n,n)) # Hessian matrix H to store approximations
+        
+        e=np.eye(n) #create basis vectors
+
+        h=self.finite_differences_step
+        
+        
+        for i in range(n): # Loop through Hessian matrix entries and approximate entries
+            for j in range(n):
+                H[i, j] = (f(x+e[i]*h+e[j]*h)-f(x+e[i]*h)-f(x+e[j]*h)+f(x))/h**2
+         
+        H = 1/2*(H + H.T) # Symmetric step
+
+        return H
+    
+    def step(self, x, s):
+        return x + s
+
