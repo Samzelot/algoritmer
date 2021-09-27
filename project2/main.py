ac6aa0e1f592561f20e9801dcb5ff6e3439e9de7
Samuel Selleck <sellecksamuel@gmail.com>
2021-09-27 13:46:10 +0200

    final form!

--- project2/main.py
+++ project2/main.py
@@ -13,7 +13,7 @@ class LoggingHessian(HessianStrategy):
 
     def __init__(self):
         self.H_1_strategy = FiniteDifferenceHessian()
-        self.H_2_strategy = DFP_rank_2_Hessian()
+        self.H_2_strategy = BFGS_rank_2_Hessian()
         self.log = []
 
     def hessian(self, problem, globals, x):
@@ -27,9 +27,11 @@ class LoggingHessian(HessianStrategy):
         return self.log
 
 def main():
-    #TODO
-    #Need to handle singular matrices
+    task_5()
+    task_7()
     task_10()
+    task_12()
+
 
 def task_12():
     #Problem
@@ -73,11 +75,11 @@ def task_5():
     f = lambda x: 100*(x[1]-x[0]**2)**2 + (1-x[0])**2
     problem = Problem(f)
 
-    hessian = DFP_rank_2_Hessian()
+    hessian = FiniteDifferenceHessian()
     params = GlobalParams(1e-5)
     stop = CauchyStopping(1e-5)
     line_solver = QuasiNewtonSolver(hessian, DefaultStep(), stop, params)
-    solver = QuasiNewtonSolver(hessian, DefaultStep(), stop, params)
+    solver = QuasiNewtonSolver(hessian, ExactLineStep(line_solver), stop, params)
     val, points = solver.solve(problem, np.array([0, -0.75]), debug=True)
     plot_countour(f, 100, -0.5, 2, -1.5, 4)
     plt.plot(*np.asarray(points).T, ".")
